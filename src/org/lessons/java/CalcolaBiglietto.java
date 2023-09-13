928ca8a0442455fef007ebd027623a7e438dbfa6
alecasentini <ale.casentini@gmail.com>
2023-09-13 16:32:34 +0200

    esercizio completato

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,5 +1,31 @@
 package org.lessons.java;
 
-public class CalcolaBiglietto {
+import java.util.Scanner;
 
+public class CalcolaBiglietto {
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 
+		 System.out.print("Inserisci il numero di Km da percorrere: ");
+			int distanza = sc.nextInt();
+			
+		 System.out.print("Inserisci la tua età: ");
+			int eta = sc.nextInt();
+			
+			sc.close();
+			
+			double prezzoBiglietto = 0;
+			
+			if(eta < 18) {
+			
+			prezzoBiglietto= distanza * 0.21 * 0.80;
+			} else if (eta >= 65) {
+			 prezzoBiglietto = distanza * 0.21 * 0.60;
+			} else {
+			prezzoBiglietto = distanza * 0.21;
+			}
+			
+			System.out.printf("Il prezzo del tuo biglietto è: €%.2f", prezzoBiglietto);
+			
+	}
 }
